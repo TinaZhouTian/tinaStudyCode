7d8e225d85602774763a41afa0baec6821844b62
TinaZhouTian <z15109550010@163.com>
2024-01-22 16:02:32 +0800

    1.22

--- springboot-demo/src/main/java/com/example/springbootdemo/controller/LiKouTest2Controller.java
+++ springboot-demo/src/main/java/com/example/springbootdemo/controller/LiKouTest2Controller.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("LiKouTest")
+@RequestMapping("LiKouTest2")
 public class LiKouTest2Controller {
 
     @Autowired
